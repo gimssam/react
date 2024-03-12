1b0081972a05c3e149a04472505e35385a0d2b95
MacbookPro <mc@MacbookcBookPro.T-mobile.com>
2024-03-13 00:21:51 +0900

    커밋

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Apple Clone 가변형 REACT Single Application Page
 ### V3. Browser Router 사용# react-test
 # apple
+
+  "homepage": "https://gimssam.github.io/apple",
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Search from './LinkComponent/Search'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path = "/" element = { <Home/> } />
         <Route path = "/discover" element = { <Discover/> } />
